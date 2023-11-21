00976f8074df65e0fe71fe0f74ae0899ac7c98d1
nicolas esteve <ganachon@hotmail.com>
2023-11-21 17:08:10 +0100

    travail suyr backend

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,19 +1,17 @@
 class DashboardsController < ApplicationController
 
-
   def show
     @keys = ["name",
-     "price",
-     "region",
-     "domain",
-     "description",
-     "year",
-     "stock_quantity",
-     "category",
-     "grape_variety",
-     "alcohol_level" ]
+            "price",
+            "region",
+            "domain",
+            "description",
+            "year",
+            "stock_quantity",
+            "category",
+            "grape_variety",
+            "alcohol_level"
+            ]
   end
-
-  private
 
 end
