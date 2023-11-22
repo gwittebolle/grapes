364a5de37a1bfcf57cf8278cd17ce0dad812dc78
Gaetan Wittebolle <gaetan.wittebolle@gmail.com>
2023-11-22 12:32:49 +0100

    new & create bookings

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -6,19 +6,12 @@ export default class extends Controller {
   static targets = ["search", "card"]
 
   connect() {
-    console.log(this.element)
-    console.log(this.searchTarget)
-    console.log(this.cardTargets)
   }
 
   search() {
     const query = this.searchTarget.value.toLowerCase();
-    console.log(query)
-
-  // Afficher ou masquer les cartes en fonction du résultat de la recherche
+    // Afficher ou masquer les cartes en fonction du résultat de la recherche
   this.cardTargets.forEach((card) => {
-    console.log(card)
-    console.log(card.innerText.toLowerCase().includes(query))
     card.innerText.toLowerCase().includes(query) ? card.classList.remove("d-none") : card.classList.add("d-none");
   });
   }
