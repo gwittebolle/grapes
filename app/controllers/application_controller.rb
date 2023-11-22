0115ad1c004792ec2b622646bf0f45990e0ae71f
Gaetan Wittebolle <gaetan.wittebolle@gmail.com>
2023-11-22 16:27:19 +0100

    geocoding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :adress, :phone_number, :password, :avatar)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :address, :phone_number, :password, :avatar)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :address, :phone_number, :password, :avatar, :current_password)}
   end
 end
--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,6 +3,15 @@ class WinesController < ApplicationController
 
   def index
     @wines = Wine.all
+    @users = User.all
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude,
+        info_window_html: render_to_string(partial: "pages/info_window", locals: {user: user}),
+        marker_html: render_to_string(partial: "shared/marker")
+      }
+    end
   end
 
   def show
