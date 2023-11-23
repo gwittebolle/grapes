6f5ead9d93d1306b280aa71c60a5c9a3deb224a6
nicolas esteve <ganachon@hotmail.com>
2023-11-23 14:05:38 +0100

    travail sur le formulaire des vins, sur l'edit et l'update du formulaire

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,12 +3,16 @@ class Wine < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
-  validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :domain, presence: true
-  validates :year, presence: true
-  validates :category, presence: true
+  REGION = ['Alsace', 'Bordeaux', 'Bourgogne', 'Champagne', 'Corse', 'Midi', 'Provence', 'Sud-ouest', 'Vallée de la Loire', 'Vallée du Rhône']
 
+  validates :name, presence: true
+  validates :region, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :year, presence: true
+  validates :stock_quantity, presence: true
+  validates :category, presence: true
+  validates :region, inclusion: { in: REGION }
+  validates :category, inclusion: { in: %w[Blanc Rouge Rosé Autre] }
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :wines, only: %i[index show new create destroy] do
+  resources :wines, only: %i[index show new create destroy edit update] do
     resources :bookings, only: %i[new create]
   end
   get 'profile', action: :show, controller: 'dashboards'
 
   # Defines the root path route ("/")
-resources :bookings, only: :destroy
+  resources :bookings, only: :destroy
   # root "posts#index"
 end
